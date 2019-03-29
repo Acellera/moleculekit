db979b20a61dba19011bd1ce9ea6a0f365deb723
Stefan Doerr <stefan.doerr@upf.edu>
2019-03-29 11:57:06 +0100

    switch to main pypi server. fix versioning of conda packages

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -2,5 +2,10 @@
 
 # Build for conda from PyPI
 source deactivate test
+
+export TAG_DESCRIBE=$(git describe)
+export BUILD_VERSION=$(echo $TAG_DESCRIBE | sed 's/-/ /g'  | awk '{print $1}')
+export BUILD_NUMBER=0
+
 conda build package/moleculekit/
 bash -x ./ci/travis/conda_upload.sh;
