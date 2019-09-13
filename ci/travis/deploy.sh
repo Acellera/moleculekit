a275903edc1cda8ae41df6e4fcdffb3958eb7b3a
Stefan Doerr <stefan.doerr@upf.edu>
2019-09-13 16:08:57 +0300

    specify exact python version for conda build

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 # Build for conda from PyPI
 source deactivate test
@@ -7,5 +8,6 @@ export TAG_DESCRIBE=$(git describe)
 export BUILD_VERSION=$(echo $TAG_DESCRIBE | sed 's/-/ /g'  | awk '{print $1}')
 export BUILD_NUMBER=0
 
-conda build package/moleculekit/
+conda build --python $(CONDA_PY) package/moleculekit/
+
 bash -x ./ci/travis/conda_upload.sh;
