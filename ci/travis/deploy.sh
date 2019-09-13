b1ab7a25d805c6352b4bf819f389011902c50b55
Stefan Doerr <stefan.doerr@upf.edu>
2019-09-13 17:12:13 +0300

    fixed conda builds

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 # Build for conda from PyPI
 source deactivate test
@@ -8,6 +7,6 @@ export TAG_DESCRIBE=$(git describe)
 export BUILD_VERSION=$(echo $TAG_DESCRIBE | sed 's/-/ /g'  | awk '{print $1}')
 export BUILD_NUMBER=0
 
-conda build --python $(CONDA_PY) package/moleculekit/
+conda build --python $CONDA_PY package/moleculekit/
 
 bash -x ./ci/travis/conda_upload.sh;
