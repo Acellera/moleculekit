a3e65423b3f4ca721b8673447084b123a017a559
Stefan Doerr <stefdoerr@gmail.com>
2020-04-21 14:00:38 +0300

    updated miniconda installation according to travis blog

--- ci/travis/install_miniconda.sh
+++ ci/travis/install_miniconda.sh
@@ -13,20 +13,18 @@ else # if it does not exist, we need to install miniconda
     if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
         curl -S https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > miniconda.sh;
     else
-        curl -S https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > miniconda.sh;
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
     fi
 
     bash miniconda.sh -b -p "$MINICONDA_DIR"
-    chown -R "$USER" "$MINICONDA_DIR"
-    export PATH="$MINICONDA_DIR/bin:$PATH"
+    source "$MINICONDA_DIR/etc/profile.d/conda.sh"
     hash -r
+
     conda config --set always_yes yes \
         --set changeps1 no \
         --set quiet yes \
         --set auto_update_conda false \
         --system # important to write to system cfg, otherwise we loose the changes upon cache reloading.
-    #conda config --system --add channels conda-forge
-    #conda install conda
 fi
 
 # we want to have an up to date conda-build.
