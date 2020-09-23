e69b8f99efdb37eea8cd4d98ecdc4f95896564fb
Stefan Doerr <stefdoerr@gmail.com>
2020-09-23 11:16:59 +0300

    added conda-forge for building

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -7,6 +7,6 @@ export TAG_DESCRIBE=$(git describe)
 export BUILD_VERSION=$(echo $TAG_DESCRIBE | sed 's/-/ /g'  | awk '{print $1}')
 export BUILD_NUMBER=0
 
-conda build --python $CONDA_PY package/moleculekit/
+conda build --python $CONDA_PY package/moleculekit/ -c conda-forge
 
 bash -x ./ci/travis/conda_upload.sh;
