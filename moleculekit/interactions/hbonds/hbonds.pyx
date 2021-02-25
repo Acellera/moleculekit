07f04de88e374e7e8898d48c4de630bc1ba713ee
Stefan Doerr <stefdoerr@gmail.com>
2021-02-25 16:52:34 +0200

    Hbonds calculator (#69)
    * added the files
    
    * added hbond calculator as module
    
    * draw from the hydrogen
    
    * add cython to reqs
    
    * install cython before installing moleculekit
    
    * try splitting the envs
    
    * build the extension in place
    
    * added install instructions
    
    * remove the built so

---
+++ moleculekit/interactions/hbonds/hbonds.pyx
@@ -0,0 +1,120 @@
+# cython: cdivision=True
+
+import numpy as np
+from math import sqrt
+cimport numpy as np
+from libcpp.vector cimport vector
+from libc.math cimport round, sqrt, acos
+
+# We now need to fix a datatype for our arrays. I've used the variable
+# DTYPE for this, which is assigned to the usual NumPy runtime
+# type info object.
+UINT32 = np.uint32
+FLOAT32 = np.float32
+
+# "ctypedef" assigns a corresponding compile-time type to DTYPE_t. For
+# every type in the numpy module there's a corresponding compile-time
+# type with a _t-suffix.
+ctypedef np.uint32_t UINT32_t
+ctypedef np.float32_t FLOAT32_t
+
+import cython
+@cython.boundscheck(False) # turn off bounds-checking for entire function
+@cython.wraparound(False)  # turn off negative index wrapping for entire function
+def calculate(
+        np.ndarray[UINT32_t, ndim=2] donors, 
+        np.ndarray[UINT32_t, ndim=1] acceptors, 
+        np.ndarray[FLOAT32_t, ndim=3] coords, 
+        np.ndarray[FLOAT32_t, ndim=2] box,
+        np.ndarray[UINT32_t, ndim=1] sel1, 
+        np.ndarray[UINT32_t, ndim=1] sel2, 
+        float dist_threshold=2.5,
+        float angle_threshold=120,
+    ):
+    cdef int d, a, f, i
+    cdef int n_donors = donors.shape[0]
+    cdef int n_acceptors = acceptors.shape[0]
+    cdef int n_frames = coords.shape[2]
+    cdef vector[vector[int]] results
+    cdef np.ndarray[FLOAT32_t, ndim=1] dist_vec_a = np.zeros(3, dtype=FLOAT32)
+    cdef np.ndarray[FLOAT32_t, ndim=1] dist_vec_b = np.zeros(3, dtype=FLOAT32)
+    cdef np.ndarray[FLOAT32_t, ndim=1] half_box = np.zeros(3, dtype=FLOAT32)
+    cdef FLOAT32_t dist2_a, dist2_b, dotprod, val, angle
+    cdef UINT32_t d_idx_d, d_idx_h, a_idx
+
+    dist_threshold = dist_threshold * dist_threshold
+    angle_threshold = angle_threshold / 57.29578
+
+    for f in range(n_frames):
+        results.push_back(vector[int]())
+        for i in range(3):
+            half_box[i] = box[i, f] / 2
+
+        for d in range(n_donors):
+            for a in range(n_acceptors):
+                a_idx = acceptors[a]
+                d_idx_d = donors[d, 0]
+                d_idx_h = donors[d, 1]
+
+                # Don't calculate h-bonds with identical donor-acceptor atom
+                if a_idx == d_idx_d:
+                    continue
+
+                # Donor and acceptor must belong to different selections
+                if sel1[a_idx] != 0:
+                    if sel2[d_idx_d] == 0:
+                        continue
+                if sel1[d_idx_d] != 0:
+                    if sel2[a_idx] == 0:
+                        continue
+
+                dist2_a = 0
+                dist2_b = 0
+                # Calculate donor hydrogen to acceptor vector
+                for i in range(3):
+                    val = coords[a_idx, i, f] - coords[d_idx_h, i, f] 
+                    # Wrap the distance vector into the periodic box
+                    if abs(val) > half_box[i] and box[i, f] != 0:
+                        val = val - box[i, f] * round(val / box[i, f])
+                    dist_vec_a[i] = val
+                    dist2_a = dist2_a + (val * val)
+
+                # Donor hydrogen too far from acceptor
+                if dist2_a > dist_threshold:
+                    continue
+
+                # Calculate donor heavy to donor hydrogen vector for the angle
+                for i in range(3):
+                    val = coords[d_idx_d, i, f] - coords[d_idx_h, i, f] 
+                    # Wrap the distance vector into the periodic box
+                    if abs(val) > half_box[i] and box[i, f] != 0:
+                        val = val - box[i, f] * round(val / box[i, f])
+                    dist_vec_b[i] = val
+                    dist2_b = dist2_b + (val * val)
+
+                # Overlapping atoms? Weird case but best exit
+                if dist2_a == 0 or dist2_b == 0:
+                    continue
+
+                # Calculate the angle
+                # Calculate vector dot product
+                dotprod = 0
+                for i in range(3):
+                    dotprod = dotprod + dist_vec_a[i] * dist_vec_b[i]
+
+                angle = dotprod / (sqrt(dist2_a) * sqrt(dist2_b))
+                # Prevent numerical issues with acos
+                if angle > 1:
+                    angle = 1
+                if angle < -1:
+                    angle = -1
+                angle = acos(angle)
+
+                if angle > angle_threshold:
+                    results[f].push_back(d_idx_d)
+                    results[f].push_back(d_idx_h)
+                    results[f].push_back(a_idx)
+
+    return results
+
+
---
+++ moleculekit/interactions/interactions.py
@@ -0,0 +1,131 @@
+import networkx as nx
+import numpy as np
+from moleculekit.tools.moleculechecks import isProteinProtonated
+
+
+def get_donors_acceptors(mol, exclude_water=True, exclude_backbone=False):
+    if not isProteinProtonated(mol):
+        raise RuntimeError(
+            "The protein seems to not be protonated. You must provide a protonated system for H-bonds to be detected."
+        )
+
+    mol_g = nx.Graph()
+    mol_g.add_edges_from(mol.bonds)
+
+    donor_elements = ("N", "O")
+    acceptor_elements = ("N", "O")
+    potential_donors = np.in1d(mol.element, donor_elements)
+    potential_acceptors = np.in1d(mol.element, acceptor_elements)
+
+    backbone = mol.atomselect("protein and backbone")
+    # Amide nitrogens are only donors
+    potential_acceptors[(mol.element == "N") & backbone] = False
+
+    if exclude_water:
+        waters = mol.atomselect("water")
+        potential_donors = ~waters & potential_donors
+        potential_acceptors = ~waters & potential_acceptors
+
+    if exclude_backbone:
+        potential_donors = ~backbone & potential_donors
+        potential_acceptors = ~backbone & potential_acceptors
+
+    if not np.any(potential_donors):
+        return
+
+    hydrogens = tuple(np.where(mol.element == "H")[0])
+    donor_pairs = []
+    for pot_do in np.where(potential_donors)[0]:
+        neighbours = np.array(list(mol_g.neighbors(pot_do)))
+        h_neighbours = neighbours[np.in1d(neighbours, hydrogens)]
+        for hn in h_neighbours:
+            donor_pairs.append([pot_do, hn])
+
+    acceptors = np.where(potential_acceptors)[0]
+
+    if not len(donor_pairs) or not len(acceptors):
+        return
+    return np.vstack(donor_pairs).astype(np.uint32), acceptors.astype(np.uint32)
+
+
+def hbonds_calculate(
+    mol,
+    donors,
+    acceptors,
+    sel1="all",
+    sel2=None,
+    dist_threshold=2.5,
+    angle_threshold=120,
+):
+    from moleculekit.interactions import hbonds
+
+    sel1 = mol.atomselect(sel1).astype(np.uint32).copy()
+    if sel2 is None:
+        sel2 = sel1.copy()
+    else:
+        sel2 = mol.atomselect(sel2).astype(np.uint32).copy()
+
+    hb = hbonds.calculate(donors, acceptors, mol.coords, mol.box, sel1, sel2, 2.5, 120)
+
+    hbond_list = []
+    for f in range(mol.numFrames):
+        hbond_list.append(np.array(hb[f]).reshape(-1, 3))
+    return hbond_list
+
+
+def view_hbonds(mol, hbonds):
+    from moleculekit.vmdgraphics import VMDCylinder
+
+    viewname = "mol"
+    if mol.viewname is not None:
+        viewname = mol.viewname.copy()
+
+    for f in range(mol.numFrames):
+        mol.viewname = f"{viewname}_frame_{f}"
+        mol.view()
+        for i in range(hbonds[f].shape[0]):
+            VMDCylinder(
+                mol.coords[hbonds[f][i, 1], :, f],
+                mol.coords[hbonds[f][i, 2], :, f],
+                radius=0.1,
+            )
+    mol.viewname = viewname
+
+
+import unittest
+
+
+class _TestInteractions(unittest.TestCase):
+    def test_hbonds(self):
+        from moleculekit.home import home
+        from moleculekit.molecule import Molecule
+        from moleculekit.tools.preparation import proteinPrepare
+        from moleculekit.smallmol.smallmol import SmallMol
+        import os
+
+        prot = os.path.join(home(dataDir="test-interactions"), "3PTB_prepared.pdb")
+        lig = os.path.join(home(dataDir="test-interactions"), "BEN.sdf")
+
+        mol = Molecule(prot)
+        lig = SmallMol(lig).toMolecule()
+        mol.append(lig)
+
+        # TODO: Add check if bonds exist in molecule! Add it to moleculechecks
+        mol.bonds = mol._guessBonds()
+        mol.coords = np.tile(mol.coords, (1, 1, 2)).copy()  # Fake second frame
+        donors, acceptors = get_donors_acceptors(
+            mol, exclude_water=True, exclude_backbone=False
+        )
+
+        hb = hbonds_calculate(mol, donors, acceptors, "protein", "resname BEN")
+        assert len(hb) == 2
+        ref = np.array([[3229, 3236, 2472], [3230, 3237, 2473], [3230, 3238, 2483]])
+        assert np.array_equal(hb[0], ref) and np.array_equal(hb[1], ref)
+
+        hb = hbonds_calculate(mol, donors, acceptors, "all")
+        assert len(hb) == 2
+        assert hb[0].shape == (179, 3)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
