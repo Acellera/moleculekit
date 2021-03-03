c38d5d304fec65a0ca6b1b5d663be3376ecdd0a9
Stefan Doerr <stefdoerr@gmail.com>
2021-03-03 10:31:31 +0200

    fix the license headers

--- moleculekit/tools/moleculechecks.py
+++ moleculekit/tools/moleculechecks.py
@@ -1,3 +1,10 @@
+# (c) 2015-2018 Acellera Ltd http://www.acellera.com
+# All Rights Reserved
+# Distributed under HTMD Software License Agreement
+# No redistribution in whole or part
+#
+
+
 def closestDistance(mol1, mol2):
     from moleculekit.projections.util import pp_calcMinDistances
 
--- moleculekit/tools/obabel_tools/__init__.py
+++ moleculekit/tools/obabel_tools/__init__.py
@@ -1,21 +1,8 @@
-# GNU LGPL v3
-# Copyright (C) 2015-2018 Acellera
-# info@acellera.com
-
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 3 of the License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
+# (c) 2015-2018 Acellera Ltd http://www.acellera.com
+# All Rights Reserved
+# Distributed under HTMD Software License Agreement
+# No redistribution in whole or part
+#
 import sys
 
 
