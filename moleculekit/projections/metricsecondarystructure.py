d8536a2b3fed5792564e65053d9d2532d9b2a58f
Stefan Doerr <stefdoerr@gmail.com>
2021-04-26 19:53:57 +0300

    removed omnia references

--- moleculekit/projections/metricsecondarystructure.py
+++ moleculekit/projections/metricsecondarystructure.py
@@ -7,11 +7,12 @@
 from moleculekit.projections.projection import Projection
 import numpy as np
 import logging
+
 logger = logging.getLogger(__name__)
 
 
 class MetricSecondaryStructure(Projection):
-    """ Calculates the secondary structure of the protein. DSSP implementation and documentation taken from MDtraj.
+    """Calculates the secondary structure of the protein. DSSP implementation and documentation taken from MDtraj.
 
     Parameters
     ----------
@@ -45,7 +46,8 @@ class MetricSecondaryStructure(Projection):
     'CA', 'N', 'C', 'O'), such as water molecules that are listed as 'residue's
     in the topology.
     """
-    def __init__(self, sel='protein', simplified=True, integer=True):
+
+    def __init__(self, sel="protein", simplified=True, integer=True):
         super().__init__()
 
         self.sel = sel
@@ -53,43 +55,49 @@ class MetricSecondaryStructure(Projection):
         self.integer = integer
 
     def _precalculate(self, mol):
-        self._pc_ca_indices, self._pc_nco_indices, self._pc_proline_indices, self._pc_chain_ids = self._calcarrays(mol)
+        (
+            self._pc_ca_indices,
+            self._pc_nco_indices,
+            self._pc_proline_indices,
+            self._pc_chain_ids,
+        ) = self._calcarrays(mol)
 
-    def _calculateMolProp(self, mol, props='all'):
+    def _calculateMolProp(self, mol, props="all"):
         from moleculekit.util import sequenceID
+
         # Calculate all properties at once since it would be too slow otherwise to redo calculations
         res = {}
-        
+
         mol = mol.copy()
         mol.filter(self.sel, _logger=False)
 
         residues = sequenceID((mol.resid, mol.chain, mol.insertion))
 
-        backbone = mol.atomselect('backbone')
-        ca_indices = np.where(mol.name == 'CA')[0].astype(np.int32)
+        backbone = mol.atomselect("backbone")
+        ca_indices = np.where(mol.name == "CA")[0].astype(np.int32)
         chainids = mol.chain[ca_indices]
         resnames = mol.resname[ca_indices]
-        proline_indices = np.array(resnames == 'PRO', dtype=np.int32)
+        proline_indices = np.array(resnames == "PRO", dtype=np.int32)
 
         _, chain_ids = np.unique(chainids, return_inverse=True)
         chain_ids = chain_ids.astype(np.int32)
 
-        nco_indices = np.ones((residues.max()+1, 3), dtype=np.int32) * -1
-        natriums = np.where((mol.name == 'N') & backbone)[0]
-        carbons = np.where((mol.name == 'C') & backbone)[0]
-        oxygens = np.where((mol.name == 'O') & backbone)[0]
+        nco_indices = np.ones((residues.max() + 1, 3), dtype=np.int32) * -1
+        natriums = np.where((mol.name == "N") & backbone)[0]
+        carbons = np.where((mol.name == "C") & backbone)[0]
+        oxygens = np.where((mol.name == "O") & backbone)[0]
         nco_indices[residues[natriums], 0] = natriums
         nco_indices[residues[carbons], 1] = carbons
         nco_indices[residues[oxygens], 2] = oxygens
 
-        res['ca_indices'] = ca_indices
-        res['nco_indices'] = nco_indices
-        res['proline_indices'] = proline_indices
-        res['chain_ids'] = chain_ids
+        res["ca_indices"] = ca_indices
+        res["nco_indices"] = nco_indices
+        res["proline_indices"] = proline_indices
+        res["chain_ids"] = chain_ids
         return res
 
     def project(self, mol):
-        """ Project molecule.
+        """Project molecule.
 
         Parameters
         ----------
@@ -102,12 +110,12 @@ class MetricSecondaryStructure(Projection):
             An array containing the projected data.
         """
         mol = mol.copy()
-        molprops = self._getMolProp(mol, 'all')
+        molprops = self._getMolProp(mol, "all")
 
-        ca_indices = molprops['ca_indices']
-        nco_indices = molprops['nco_indices']
-        proline_indices = molprops['proline_indices']
-        chain_ids = molprops['chain_ids']
+        ca_indices = molprops["ca_indices"]
+        nco_indices = molprops["nco_indices"]
+        proline_indices = molprops["proline_indices"]
+        chain_ids = molprops["chain_ids"]
 
         mol.filter(self.sel, _logger=False)
 
@@ -117,41 +125,52 @@ class MetricSecondaryStructure(Projection):
         try:
             from mdtraj.geometry._geometry import _dssp as dssp
         except ImportError:
-            raise ImportError('To calculate secondary structure you need to install mdtraj with `conda install mdtraj -c omnia`')
+            raise ImportError(
+                "To calculate secondary structure you need to install mdtraj with `conda install mdtraj -c conda-forge`"
+            )
 
-        natoms = np.unique((nco_indices.shape[0], ca_indices.shape[0], proline_indices.shape[0], chain_ids.shape[0]))
+        natoms = np.unique(
+            (
+                nco_indices.shape[0],
+                ca_indices.shape[0],
+                proline_indices.shape[0],
+                chain_ids.shape[0],
+            )
+        )
         if len(natoms) != 1:
-            raise AssertionError('Wrong dimensions in SS data arrays. Report this bug on the issue tracker.')
+            raise AssertionError(
+                "Wrong dimensions in SS data arrays. Report this bug on the issue tracker."
+            )
         data = dssp(xyz, nco_indices, ca_indices, proline_indices, chain_ids)
 
         if self.simplified:
-            trans = str.maketrans('HGIEBTS ', 'HHHEECCC')
+            trans = str.maketrans("HGIEBTS ", "HHHEECCC")
             data = data.translate(trans)
 
-        data = np.fromiter(data, dtype=np.dtype('U2'))
+        data = np.fromiter(data, dtype=np.dtype("U2"))
         data = data.reshape(mol.numFrames, len(chain_ids))
 
         if self.integer:
             if self.simplified:
-                data[data == 'H'] = 2
-                data[data == 'E'] = 1
-                data[data == 'C'] = 0
+                data[data == "H"] = 2
+                data[data == "E"] = 1
+                data[data == "C"] = 0
             else:
-                data[data == 'H'] = 3
-                data[data == 'B'] = 4
-                data[data == 'E'] = 5
-                data[data == 'G'] = 6
-                data[data == 'I'] = 7
-                data[data == 'T'] = 8
-                data[data == 'S'] = 9
-                data[data == ' '] = 10
+                data[data == "H"] = 3
+                data[data == "B"] = 4
+                data[data == "E"] = 5
+                data[data == "G"] = 6
+                data[data == "I"] = 7
+                data[data == "T"] = 8
+                data[data == "S"] = 9
+                data[data == " "] = 10
             data = np.array(data, dtype=np.int32)
-        #data[:, np.logical_not(protein_indices)] = 'NA'
+        # data[:, np.logical_not(protein_indices)] = 'NA'
 
         return data
 
     def getMapping(self, mol):
-        """ Returns the description of each projected dimension.
+        """Returns the description of each projected dimension.
 
         Parameters
         ----------
@@ -163,16 +182,23 @@ class MetricSecondaryStructure(Projection):
         map : :class:`DataFrame <pandas.core.frame.DataFrame>` object
             A DataFrame containing the descriptions of each dimension
         """
-        idx = mol.atomselect('{} and name CA'.format(self.sel), indexes=True)
+        idx = mol.atomselect("{} and name CA".format(self.sel), indexes=True)
         from pandas import DataFrame
+
         types = []
         indexes = []
         description = []
         for i in idx:
-            types += ['secondary structure']
+            types += ["secondary structure"]
             indexes += [i]
-            description += ['Secondary structure of residue {} {}{}'.format(mol.resname[i], mol.resid[i], mol.insertion[i])]
-        return DataFrame({'type': types, 'atomIndexes': indexes, 'description': description})
+            description += [
+                "Secondary structure of residue {} {}{}".format(
+                    mol.resname[i], mol.resid[i], mol.insertion[i]
+                )
+            ]
+        return DataFrame(
+            {"type": types, "atomIndexes": indexes, "description": description}
+        )
 
 
 def _ssmap(sschar):
@@ -188,18 +214,19 @@ def _ssmap(sschar):
     ssnum = np.zeros((len(sschar), len(sschar[0])), dtype=np.uint8)
 
     for i in range(len(sschar)):
-        sschar[i][sschar[i] == 'G'] = 'H'
-        sschar[i][sschar[i] == 'I'] = 'H'
-        sschar[i][sschar[i] == 'B'] = 'E'
-        sschar[i][sschar[i] == 'b'] = 'E'
-        sschar[i][sschar[i] == 'S'] = 'C'
-        sschar[i][sschar[i] == 'T'] = 'C'
-        ssnum[i, np.where(sschar[i] == 'C')[0]] = 0
-        ssnum[i, np.where(sschar[i] == 'E')[0]] = 1
-        ssnum[i, np.where(sschar[i] == 'H')[0]] = 2
+        sschar[i][sschar[i] == "G"] = "H"
+        sschar[i][sschar[i] == "I"] = "H"
+        sschar[i][sschar[i] == "B"] = "E"
+        sschar[i][sschar[i] == "b"] = "E"
+        sschar[i][sschar[i] == "S"] = "C"
+        sschar[i][sschar[i] == "T"] = "C"
+        ssnum[i, np.where(sschar[i] == "C")[0]] = 0
+        ssnum[i, np.where(sschar[i] == "E")[0]] = 1
+        ssnum[i, np.where(sschar[i] == "H")[0]] = 2
     return ssnum
 
 
+# fmt: off
 import unittest
 class _TestMetricSecondaryStructure(unittest.TestCase):
     def test_2hbb(self):
@@ -243,8 +270,6 @@ class _TestMetricSecondaryStructure(unittest.TestCase):
         assert np.array_equal(data, ref), 'MetricSecondaryStructure assertion failed'
         assert MetricSecondaryStructure().getMapping(mol).shape == (223, 3)
 
-
+# fmt: on
 if __name__ == "__main__":
     unittest.main(verbosity=2)
-
-
