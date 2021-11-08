69d97e7e0ec23554d733fffb939bc58fd4960b1c
Stefan Doerr <stefdoerr@gmail.com>
2021-11-08 17:15:29 +0200

    refactored sequence based alignment algorithm to work better for protein and nucleics. added tests. Molecule.sequence now returns residue atom indexes and accepts atomselections

--- moleculekit/tools/sequencestructuralalignment.py
+++ moleculekit/tools/sequencestructuralalignment.py
@@ -5,46 +5,58 @@
 #
 import numpy as np
 import logging
+from moleculekit.molecule import Molecule
+import unittest
 
 logger = logging.getLogger(__name__)
 
 
-def _get_sequence(mol, segs, protein_mask, nucleic_mask):
-    from moleculekit.util import ensurelist
+def _get_sequence(mol: Molecule, sel):
+    sel_mask = mol.atomselect(sel)
+    protein_mask = mol.atomselect("protein")
+    nucleic_mask = mol.atomselect("nucleic")
+    if any(protein_mask & sel_mask) and any(nucleic_mask & sel_mask):
+        raise RuntimeError(
+            "Your selection contains both protein and nucleic residues. You need to clarify which selection to align."
+        )
+    molseg = "protein" if any(protein_mask) else "nucleic"
+    seqmol, seqidx = mol.sequence(noseg=True, return_idx=True, sel=sel)
+    seqidx = seqidx[molseg]
+    seqmol = seqmol[molseg]
+    segment_type = molseg
+    return seqmol, seqidx, segment_type
 
-    if segs is None:
-        if any(protein_mask) and any(nucleic_mask):
-            raise RuntimeError(
-                "mol contains both protein and nucleic residues. You need to specify which segments to align."
-            )
-        molseg = "protein" if any(protein_mask) else "nucleic"
-        seqmol = mol.sequence(noseg=True)[molseg]
-        segment_type = molseg
-    else:
-        seqs = mol.sequence()
-        seqmol = ""
-        has_prot = False
-        has_nucl = False
-        for seg in ensurelist(segs):
-            seg_is_prot = any(protein_mask & (mol.segid == seg))
-            seg_is_nucl = any(nucleic_mask & (mol.segid == seg))
-            if seg_is_prot and seg_is_nucl:
-                raise RuntimeError(
-                    f"Segment {seg} of mol contains both protein and nucleic atoms. Please reassign segments."
-                )
-            has_prot = has_prot | seg_is_prot
-            has_nucl = has_nucl | seg_is_nucl
-            seqmol += seqs[seg]
-        if has_prot and has_nucl:
-            raise RuntimeError(
-                "The segments you provided contain both protein and nucleic atoms. Please choose different segments"
-            )
-        segment_type = "protein" if has_prot else "nucleic"
-    return seqmol, segment_type
+
+def _get_max_align_region(molaln, refaln, nalignfragment):
+    dsig = np.hstack(([False], (refaln != "-") & (molaln != "-"), [False])).astype(int)
+    dsigdiff = np.diff(dsig)
+    startIndex = np.where(dsigdiff > 0)[0]
+    endIndex = np.where(dsigdiff < 0)[0]
+    duration = endIndex - startIndex
+    duration_sorted = np.sort(duration)[::-1]
+
+    _list_starts = []
+    _list_finish = []
+    for n in range(nalignfragment):
+        if n == len(duration):
+            break
+        idx = np.where(duration == duration_sorted[n])[0]
+        start = startIndex[idx][0]
+        finish = endIndex[idx][0]
+        _list_starts.append(start)
+        _list_finish.append(finish)
+    return _list_starts, _list_finish
 
 
 def sequenceStructureAlignment(
-    mol, ref, molseg=None, refseg=None, maxalignments=10, nalignfragment=1
+    mol,
+    ref,
+    molseg=None,
+    refseg=None,
+    molsel="all",
+    refsel="all",
+    maxalignments=10,
+    nalignfragment=1,
 ):
     """Aligns two structures by their longests sequences alignment
 
@@ -70,7 +82,16 @@ def sequenceStructureAlignment(
     masks : list
         A list of boolean mask pairs of which atoms were aligned. [[molmask1, refmask1], [molmask2, refmask2], ...]
     """
-    from moleculekit.util import ensurelist
+    if molseg is not None:
+        logger.warning(
+            'molseg argument is deprecated. Please use molsel="segid X" instead.'
+        )
+        molsel = f"segid {molseg}"
+    if refseg is not None:
+        logger.warning(
+            'refseg argument is deprecated. Please use refsel="segid X" instead.'
+        )
+        refsel = f"segid {refseg}"
 
     try:
         from Bio import pairwise2
@@ -89,52 +110,13 @@ def sequenceStructureAlignment(
             "Alternative atom locations detected in `ref`. Please remove these before calling this function."
         )
 
-    mol_protein = mol.atomselect("protein")
-    mol_nucleic = mol.atomselect("nucleic")
-    ref_protein = ref.atomselect("protein")
-    ref_nucleic = ref.atomselect("nucleic")
-
-    seqmol, segment_type_mol = _get_sequence(mol, molseg, mol_protein, mol_nucleic)
-    seqref, segment_type_ref = _get_sequence(ref, refseg, ref_protein, ref_nucleic)
+    seqmol, molidx, segment_type_mol = _get_sequence(mol, molsel)
+    seqref, refidx, segment_type_ref = _get_sequence(ref, refsel)
     if segment_type_mol != segment_type_ref:
         raise RuntimeError(
             f"The segments of mol are of type {segment_type_mol} while for ref they are of type {segment_type_ref}. Please choose different segments to align."
         )
     segment_type = segment_type_mol
-
-    for seg in seqref:
-        seg_is_prot = any(ref_protein & (ref.segid == seg))
-        seg_is_nucl = any(ref_nucleic & (ref.segid == seg))
-        if seg_is_prot and seg_is_nucl:
-            raise RuntimeError(
-                f"Segment {seg} of ref contains both protein and nucleic atoms. Please reassign segments."
-            )
-
-    def getSegIdx(m, seg, segment_type):
-        # Calculate the atoms which belong to the selected segments
-        if seg is None and segment_type == "protein":
-            msegidx = m.atomselect("protein and name CA")
-        elif seg is None and segment_type == "nucleic":
-            msegidx = m.atomselect("nucleic and backbone and name P")
-        else:
-            msegidx = np.zeros(m.numAtoms, dtype=bool)
-            atom = "CA" if segment_type == "protein" else "P"
-            for seg in ensurelist(seg):
-                msegidx |= (m.segid == seg) & (m.name == atom)
-        return np.where(msegidx)[0]
-
-    molsegidx = getSegIdx(mol, molseg, segment_type)
-    refsegidx = getSegIdx(ref, refseg, segment_type)
-
-    # Create fake residue numbers for the selected segment
-    from moleculekit.util import sequenceID
-
-    molfakeresid = sequenceID(
-        (mol.resid[molsegidx], mol.insertion[molsegidx], mol.chain[molsegidx])
-    )
-    reffakeresid = sequenceID(
-        (ref.resid[refsegidx], ref.insertion[refsegidx], ref.chain[refsegidx])
-    )
 
     if segment_type == "protein":
         alignments = pairwise2.align.globaldx(seqref, seqmol, matlist.blosum62)
@@ -145,6 +127,19 @@ def sequenceStructureAlignment(
     if numaln > maxalignments:
         logger.warning(
             f"{numaln} alignments found. Limiting to {maxalignments} as specified in the `maxalignments` argument."
+        )
+
+    def _find_common_atom(mol, ref, idxmol, idxref):
+        moln = mol.name[idxmol].tolist()
+        refn = ref.name[idxref].tolist()
+        prot_names = ["CA", "C", "O", "N"]
+        nucl_names = ["C3'", "C4'", "C1'", "P"]
+        names = prot_names if segment_type == "protein" else nucl_names
+        for name in names:
+            if name in moln and name in refn:
+                return idxmol[moln.index(name)], idxref[refn.index(name)]
+        raise RuntimeError(
+            f"Could not find any of {names} in both mol and ref residues"
         )
 
     alignedstructs = []
@@ -158,65 +153,30 @@ def sequenceStructureAlignment(
         residmol = np.cumsum(molaln != "-") - 1  # Start them from 0
 
         # Find the region of maximum alignment between the molecules
-        dsig = np.hstack(([False], (refaln != "-") & (molaln != "-"), [False])).astype(
-            int
-        )
-        dsigdiff = np.diff(dsig)
-        startIndex = np.where(dsigdiff > 0)[0]
-        endIndex = np.where(dsigdiff < 0)[0]
-        duration = endIndex - startIndex
-        duration_sorted = np.sort(duration)[::-1]
+        starts, ends = _get_max_align_region(molaln, refaln, nalignfragment)
 
-        _list_starts = []
-        _list_finish = []
-        for n in range(nalignfragment):
-            if n == len(duration):
-                break
-            idx = np.where(duration == duration_sorted[n])[0]
-            start = startIndex[idx][0]
-            finish = endIndex[idx][0]
-            _list_starts.append(start)
-            _list_finish.append(finish)
-
-        # Get the "resids" of the aligned residues only
-        refalnresid = np.concatenate(
-            [
-                residref[start:finish]
-                for start, finish in zip(_list_starts, _list_finish)
-            ]
-        )
-        molalnresid = np.concatenate(
-            [
-                residmol[start:finish]
-                for start, finish in zip(_list_starts, _list_finish)
-            ]
-        )
-        refidx = []
-        for r in refalnresid:
-            refidx += list(refsegidx[reffakeresid == r])
-        molidx = []
-        for r in molalnresid:
-            molidx += list(molsegidx[molfakeresid == r])
+        _refidx = []
+        _molidx = []
+        startends = []
+        for ss, ff in zip(starts, ends):
+            for k, (rr, mr) in enumerate(zip(residref[ss:ff], residmol[ss:ff])):
+                mi, ri = _find_common_atom(mol, ref, molidx[mr], refidx[rr])
+                _refidx.append(ri)
+                _molidx.append(mi)
+                # The rest is just for pretty printing
+                if k == 0:
+                    start_resid = mol.resid[mi]
+                if k == ff - ss - 1:
+                    end_resid = mol.resid[mi]
+            startends.append(f"{start_resid}-{end_resid}")
 
         molmask = np.zeros(mol.numAtoms, dtype=bool)
-        molmask[molidx] = True
+        molmask[_molidx] = True
         refmask = np.zeros(ref.numAtoms, dtype=bool)
-        refmask[refidx] = True
+        refmask[_refidx] = True
 
-        start_residues = np.concatenate(
-            [mol.resid[molsegidx[molfakeresid == residmol[r]]] for r in _list_starts]
-        )
-        finish_residues = np.concatenate(
-            [
-                mol.resid[molsegidx[molfakeresid == residmol[r - 1]]]
-                for r in _list_finish
-            ]
-        )
-        joined = ", ".join(
-            [f"{s}-{f}" for s, f in zip(start_residues, finish_residues)]
-        )
         logger.info(
-            f"Alignment #{i} was done on {len(refalnresid)} residues: mol segid {np.unique(mol.segid[molidx])[0]} resid {joined}"
+            f"Alignment #{i} was done on {len(_refidx)} residues: {' ,'.join(startends)}"
         )
 
         alignedmol = mol.copy()
@@ -225,3 +185,37 @@ def sequenceStructureAlignment(
         masks.append([molmask, refmask])
 
     return alignedstructs, masks
+
+
+class _TestSequenceStructuralAlignment(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        from moleculekit.home import home
+
+        self.home = home(dataDir="test-sequence-alignment")
+
+    def test_align_protein(self):
+        import os
+
+        mol1 = Molecule(os.path.join(self.home, "4OBE.pdb"))
+        mol2 = Molecule(os.path.join(self.home, "6OB2.pdb"))
+        refmol = Molecule(os.path.join(self.home, "4OBE_aligned.pdb"))
+        mol3, _ = sequenceStructureAlignment(
+            mol1, mol2, molsel="protein", refsel="protein"
+        )
+
+        assert np.allclose(refmol.coords, mol3[0].coords, atol=1e-3)
+
+    def test_align_rna(self):
+        import os
+
+        mol1 = Molecule(os.path.join(self.home, "5C45.pdb"))
+        mol2 = Molecule(os.path.join(self.home, "5C45_sim.pdb"))
+        refmol = Molecule(os.path.join(self.home, "5C45_aligned.pdb"))
+        mol3, _ = sequenceStructureAlignment(mol1, mol2)
+
+        assert np.allclose(refmol.coords, mol3[0].coords, atol=1e-3)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
