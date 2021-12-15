5fe104290fcb273e75f21d94362f6ed28914aad8
Stefan Doerr <stefdoerr@gmail.com>
2021-12-15 12:21:28 +0200

    updated all license headers

--- moleculekit/__init__.py
+++ moleculekit/__init__.py
@@ -1,3 +1,8 @@
+# (c) 2015-2022 Acellera Ltd http://www.acellera.com
+# All Rights Reserved
+# Distributed under HTMD Software License Agreement
+# No redistribution in whole or part
+#
 from moleculekit.version import version as _version
 from moleculekit.home import home as __home
 import os
