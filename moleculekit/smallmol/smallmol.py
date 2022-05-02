b76c0d05d3f14591126af9e5259b11d69fdec3a3
Alejandro <alejandrovarelarial@gmail.com>
2022-05-02 14:53:48 +0200

    adress Stefans suggestions and more

--- moleculekit/smallmol/smallmol.py
+++ moleculekit/smallmol/smallmol.py
@@ -762,29 +762,31 @@ class SmallMol(object):
 
         Returns
         -------
-        smart: str
-            The smarts string
+        The values of the different descriptors
+        The names of the different descriptors
         """
         from rdkit.Chem import Descriptors
 
         desc_dict = dict(Descriptors.descList)
         descs = list(desc_dict.keys())
-        descs.remove('Ipc')
+        descs.remove("Ipc")
         ans = {}
         for descname in descs:
             try:
                 desc = desc_dict[descname]
                 value = desc(self._mol)
             except (ValueError, TypeError, ZeroDivisionError) as exception:
-                logger.warning(f"Descriptor {descname} was not calculated for this small molecule due to {str(exception)}")
-                value = 'NaN'
+                logger.warning(
+                    f"Descriptor {descname} was not calculated for this small molecule due to {str(exception)}"
+                )
+                value = None  # "NaN"
 
-            bin_name = 'DESC_{}'.format(descname)
+            bin_name = f"DESC_{descname}"
             ans[bin_name] = value
 
-        molstring = ans.values()
-        headers = ans.keys()
-        return molstring, headers
+        descr_values = ans.values()
+        descr_headers = ans.keys()
+        return descr_values, descr_headers
 
     def fingerprint(self, radius=2, num_bits=1024):
         """
@@ -808,7 +810,7 @@ class SmallMol(object):
         ecfp4 = AllChem.GetHashedMorganFingerprint(self._mol, radius, num_bits)
         maccs = MACCSkeys.GenMACCSKeys(self._mol)
         avalons = GetAvalonCountFP(self._mol, num_bits)
-        return (ecfp4, maccs, avalons)
+        return ecfp4, maccs, avalons
 
     def featurize(self, radius, num_bits):
         """
@@ -824,23 +826,29 @@ class SmallMol(object):
         Returns
         -------
         A list with all the features and another list with the name of the property
-        """        
+        """
         descriptors, headers = self.describe()
         ecfp4, maccs, avalons = self.fingerprint(radius, num_bits)
 
-        features = [Chem.MolToSmiles(self._mol)] + [x for x in ecfp4] + [x for x in maccs] + [x for x in avalons] 
-        features.extend(descriptors)   
+        features = (
+            [Chem.MolToSmiles(self._mol)]
+            + [x for x in ecfp4]
+            + [x for x in maccs]
+            + [x for x in avalons]
+        )
+        features.extend(descriptors)
 
-        all_headers = ['smiles']
-        all_headers.extend(['ECFC4_bit_{}'.format(str(x)) for x in range(num_bits)])
-        all_headers.extend(['MACC_bit_{}'.format(str(x)) for x in range(len(maccs))])
-        all_headers.extend(['Avalon_bit_{}'.format(str(x)) for x in range(num_bits)])
+        all_headers = ["smiles"]
+        all_headers.extend([f"ECFC4_bit_{x}" for x in range(num_bits)])
+        all_headers.extend([f"MACC_bit_{x}" for x in range(len(maccs))])
+        all_headers.extend([f"Avalon_bit_{x}" for x in range(num_bits)])
         all_headers.extend(headers)
 
         return all_headers, features
 
-
-    def strip_salts_and_detect_metals(self, metals_smart='[Mg,Ca,Zn,As,Mn,Al,Pd,Pt,Co,Ba,Cr,Cu,Ni,Ag,Fe,Hg,Cd,Gd,Na]'):
+    def strip_salts_and_detect_metals(
+        self, metals_smart="[Mg,Ca,Zn,As,Mn,Al,Pd,Pt,Co,Ba,Cr,Cu,Ni,Ag,Fe,Hg,Cd,Gd,Na]"
+    ):
         from rdkit.Chem.SaltRemover import SaltRemover
 
         query = Chem.MolFromSmarts(metals_smart)
@@ -848,9 +856,9 @@ class SmallMol(object):
         self._mol = remover.StripMol(self._mol)
         if self._mol.HasSubstructMatch(query):
             logger.warning("Metal detected in this small molecule")
-            return 1
+            return True
 
-        return 0
+        return False
 
     def toSMARTS(self, explicitHs=False):
         """
--- moleculekit/tools/train_xgboost.py
+++ moleculekit/tools/train_xgboost.py
@@ -1,42 +1,101 @@
-from moleculekit.smallmol.smallmollib import SmallMolLib
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from xgboost import XGBRegressor
-import numpy as np
+import logging
 
-def train_model(train_sdf, target_prop, val_set_fraction=0.1, scale=True):
+logger = logging.getLogger(__name__)
+
+
+def train_model(
+    train_sdf, target_prop, val_set_fraction=0.1, fp_radius=2, fp_size=1024, scale=True
+):
+    """
+    Trains an xgbregressor model for all the molecules inside `train_sdf` using as label the SDF property specified in
+    `target_prop`. The molecules get automatically featurized as a combination of RDKIT computed properties, such as
+    number of rings, or number of acceptors, and a combination of 3 different fingerprints (Avalon, MACCSkeys and Morgan).
+
+    Parameters
+    ----------
+    train_sdf: str
+        path to an SDF file with molecules, must contain the target label you want to train your model on
+    target_prop: str
+        The name of the SDF property field to use as label, like "IC50" or "Activity"
+    val_set_fraction: float
+        Fraction of the molecules inside the SDF to use for model validation.
+    fp_radius: int
+        radius of the morgan fingerprint
+    fp_size: int
+        the size of the fingerprint for the Avalon and Morgan fingerprints.
+    scale: bool
+        wheter to use some form of feature scaling or not
+
+    Returns
+    -------
+    the XGBRegressor model trained
+    """
+    from moleculekit.smallmol.smallmollib import SmallMolLib
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.model_selection import train_test_split
+    from xgboost import XGBRegressor
+    import numpy as np
+    import pandas as pd
+
     vectors = []
     labels = []
+    smiles = []
     for mol in SmallMolLib(train_sdf):
         metal = mol.strip_salts_and_detect_metals()
-        
-        if metal==0: #discard molecules with metals
+
+        if metal:  # discard molecules with metals
             prop_names = [i for i in mol._mol.GetPropNames()]
             if target_prop in prop_names:
-                headers, props  = mol.featurize(2, 1024)
-                vectors.append(props)
+                headers, props = mol.featurize(fp_radius, fp_size)
+                smiles.append(props[0])
+                vectors.append(props[1:])
                 labels.append(float(mol._mol.GetProp(target_prop)))
 
-    X = np.array(vectors) 
-    y = np.array(labels)
-    X = X[:, 1:] #discard smiles column
-    X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=val_set_fraction)
+    smiles = np.array(smiles)
+    vectors = np.array(vectors)
+    labels = np.array(labels)[:, np.newaxis]
+    values = np.append(vectors, labels, axis=1)
+    headers = headers[1:]
+    headers.append("label")
+
+    df = pd.DataFrame(values, columns=headers)
+    # remove +inf, -inf and nans
+    df = df.replace([np.inf, -np.inf], np.nan)  # .dropna()
+
+    y = df.iloc[:, -1]
+    X = df.iloc[:, :-1]
+
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, y, test_size=val_set_fraction
+    )
 
     if scale:
-        #should all features be scaled? even binary values?
+        logger.info("Scaling the computed features...")
+        # should all features be scaled? even binary values?
+        # probally the function should return the scaler for inferences
         scaler = StandardScaler()
+        X_train[X_train.columns] = scaler.fit_transform(X_train[X_train.columns])
+
         X_train = scaler.fit_transform(X_train)
         X_test = scaler.transform(X_test)
 
-    model = XGBRegressor(n_estimators=1500, max_depth=5, n_jobs=-1, subsample=0.7, learning_rate=0.05, 
-                        colsample_bytree=0.7, gamma=0.1, min_child_weight=1)
+    logger.info("Training the XGB regressor...")
+    model = XGBRegressor(
+        n_estimators=1500,
+        max_depth=5,
+        n_jobs=-1,
+        subsample=0.7,
+        learning_rate=0.05,
+        colsample_bytree=0.7,
+        gamma=0.1,
+        min_child_weight=1,
+    )
     model.fit(X_train, Y_train)
 
     y_predicted = model.predict(X_test)
-    print('Corr. Coef for test set:', np.corrcoef(Y_test,y_predicted)[0][1])
-    return vectors, labels
+    print("Corr. Coef for test set:", np.corrcoef(Y_test, y_predicted)[0][1])
+    return model
 
-a, b = train_model(
-    'some.sdf',
-    'Kd'
-    )
+
+trained_model = train_model("Data_SDF.sdf", "logP")
+
