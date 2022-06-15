5955e8f23e712d2bc601aec48ebcea946e3c733b
Stefan Doerr <stefdoerr@gmail.com>
2022-06-15 12:17:16 +0300

    implemented many more atomselection syntax variants

--- moleculekit/atomselect/atomselect.py
+++ moleculekit/atomselect/atomselect.py
@@ -1,16 +1,45 @@
 import moleculekit.ply.lex as lex
 import moleculekit.ply.yacc as yacc
 
+# molecule types
 reserved = [
     "protein",
     "nucleic",
     "lipid",
+    "lipids",
     "ion",
+    "ions",
+    "water",
+    "waters",
+    "noh",
+    "hydrogen",
+    "backbone",
+    "sidechain",
+]
+# molecule properties
+reserved += [
+    "index",
+    "serial",
+    "name",
+    "element",
     "resname",
     "resid",
     "residue",
-    "index",
-    "serial",
+    "altloc",
+    "insertion",
+    "chain",
+    "segname",
+    "segid",
+    "mass",
+    "charge",
+    "occupancy",
+    "fragment",
+    # "x",
+    # "y",
+    # "z",
+]
+# operands
+reserved += [
     "and",
     "or",
     "within",
@@ -18,7 +47,10 @@ reserved = [
     "of",
     "same",
     "as",
+    "not",
 ]
+# functions
+reserved += ["abs", "sqr"]
 # List of token names.   This is always required
 tokens = [
     "PLUS",
@@ -30,6 +62,16 @@ tokens = [
     "ID",
     "INTEGER",
     "FLOAT",
+    "NORMSTRING",
+    "NORMSTRINGSINGLE",
+    "EQUAL",
+    "LESSER",
+    "GREATER",
+    "LESSEREQUAL",
+    "GREATEREQUAL",
+    "XCOOR",
+    "YCOOR",
+    "ZCOOR",
 ] + [rr.upper() for rr in reserved]
 
 # Regular expression rules for simple tokens
@@ -39,6 +81,26 @@ t_TIMES = r"\*"
 t_DIVIDE = r"/"
 t_LPAREN = r"\("
 t_RPAREN = r"\)"
+t_EQUAL = r"\="
+t_LESSER = r"\<"
+t_GREATER = r"\>"
+t_LESSEREQUAL = r"\<\="
+t_GREATEREQUAL = r"\>\="
+
+
+def t_XCOOR(t):
+    r"x"
+    return t
+
+
+def t_YCOOR(t):
+    r"y"
+    return t
+
+
+def t_ZCOOR(t):
+    r"z"
+    return t
 
 
 def t_ID(t):
@@ -46,6 +108,16 @@ def t_ID(t):
     t.type = "ID"
     if t.value.lower() in reserved:
         t.type = t.value.upper()
+    return t
+
+
+def t_NORMSTRING(t):
+    r'"(?:[^"\\]|\\.)*"'
+    return t
+
+
+def t_NORMSTRINGSINGLE(t):
+    r"'(?:[^'\\]|\\.)*'"
     return t
 
 
@@ -83,20 +155,21 @@ lexer = lex.lex()
 precedence = (
     ("left", "PLUS", "MINUS"),
     ("left", "TIMES", "DIVIDE"),
-    ("right", "UPLUS", "UMINUS"),
+    ("right", "UMINUS"),
+    ("right", "UNOT"),
 )
 
 
 # Write functions for each grammar rule which is
 # specified in the docstring.
-def p_expression_binop(p):
-    """
-    expression : expression PLUS expression
-               | expression MINUS expression
-               | expression TIMES expression
-               | expression DIVIDE expression
-    """
-    p[0] = ("binop", p[2], p[1], p[3])
+# def p_expression_binop(p):
+#     """
+#     expression : expression PLUS expression
+#                | expression MINUS expression
+#                | expression TIMES expression
+#                | expression DIVIDE expression
+#     """
+#     p[0] = ("binop", p[2], p[1], p[3])
 
 
 def p_expression_logop(p):
@@ -105,6 +178,13 @@ def p_expression_logop(p):
                | expression OR expression
     """
     p[0] = ("logop", p[2], p[1], p[3])
+
+
+def p_expression_unary_not(p):
+    """
+    expression : NOT expression %prec UNOT
+    """
+    p[0] = ("logop", "not", p[2])
 
 
 def p_expression_within(p):
@@ -126,14 +206,14 @@ def p_expression_sameas(p):
     expression : SAME molecule AS expression
                | SAME molprop AS expression
     """
-    p[0] = ("exwithin", p[2], p[4])
+    p[0] = ("sameas", p[2], p[4])
 
 
-def p_expression_id(p):
+def p_expression_grouped(p):
     """
-    expression : ID
+    expression : LPAREN expression RPAREN
     """
-    p[0] = ("name", p[1])
+    p[0] = ("grouped", p[2])
 
 
 def p_expression_molecule(p):
@@ -148,9 +228,108 @@ def p_molecule(p):
     molecule : PROTEIN
              | NUCLEIC
              | ION
+             | IONS
              | LIPID
+             | LIPIDS
+             | WATER
+             | WATERS
+             | FRAGMENT
+             | BACKBONE
+             | HYDROGEN
+             | NOH
+             | SIDECHAIN
     """
     p[0] = ("molecule", p[1])
+
+
+def p_number_expression(p):
+    """
+    expression : number
+    """
+    p[0] = p[1]
+
+
+def p_func_number(p):
+    """
+    number : func
+    """
+    p[0] = p[1]
+
+
+def p_num_funcs(p):
+    """
+    func : ABS LPAREN number RPAREN
+         | SQR LPAREN number RPAREN
+    """
+    p[0] = ("func", p[1], p[3])
+
+
+def p_prop_funcs(p):
+    """
+    func : ABS LPAREN numprop RPAREN
+         | SQR LPAREN numprop RPAREN
+    """
+    p[0] = ("func", p[1], p[3])
+
+
+def p_expression_comp(p):
+    """
+    expression : comp
+    """
+    p[0] = p[1]
+
+
+def p_prop_comp(p):
+    """
+    comp : numprop EQUAL number
+         | numprop LESSER number
+         | numprop GREATER number
+         | numprop LESSEREQUAL number
+         | numprop GREATEREQUAL number
+    """
+    p[0] = ("comp", p[2], p[1], p[3])
+
+
+def p_func_comp(p):
+    """
+    comp : func EQUAL number
+         | func LESSER number
+         | func GREATER number
+         | func LESSEREQUAL number
+         | func GREATEREQUAL number
+    """
+    p[0] = ("comp", p[2], p[1], p[3])
+
+
+def p_number_comp(p):
+    """
+    comp : number EQUAL number
+         | number LESSER number
+         | number GREATER number
+         | number LESSEREQUAL number
+         | number GREATEREQUAL number
+    """
+    p[0] = ("comp", p[2], p[1], p[3])
+
+
+def p_number_mathop(p):
+    """
+    number : number PLUS number
+           | number MINUS number
+           | number TIMES number
+           | number DIVIDE number
+    """
+    p[0] = ("mathop", p[2], p[1], p[3])
+
+
+def p_numprop_mathop(p):
+    """
+    numprop : numprop PLUS number
+            | numprop MINUS number
+            | numprop TIMES number
+            | numprop DIVIDE number
+    """
+    p[0] = ("mathop", p[2], p[1], p[3])
 
 
 def p_expression_molprop(p):
@@ -160,50 +339,103 @@ def p_expression_molprop(p):
     p[0] = p[1]
 
 
-def p_molprop_resname(p):
+def p_molprop_string(p):
     """
-    molprop : RESNAME ID
+    molprop : NAME string
+            | ELEMENT string
+            | RESNAME string
+            | ALTLOC string
+            | SEGNAME string
+            | RESNAME number
+            | SEGID string
+            | SEGID number
+            | INSERTION string
+            | INSERTION number
+            | CHAIN string
+            | CHAIN number
     """
-    p[0] = ("resname", p[2])
+    p[0] = ("molprop", p[1], str(p[2]))
 
 
-def p_molprop_resid(p):
+def p_expression_numprop(p):
     """
-    molprop : RESID number
-            | RESIDUE number
+    expression : numprop
     """
-    p[0] = (p[1], p[2])
+    p[0] = p[1]
 
 
-def p_molprop_indexserial(p):
+def p_molprop_number(p):
     """
-    molprop : INDEX number
-            | SERIAL number
+    numprop : CHARGE
+            | MASS
+            | OCCUPANCY
+            | XCOOR
+            | YCOOR
+            | ZCOOR
     """
-    p[0] = (p[1], p[2])
+    p[0] = ("numprop", p[1])
 
 
-def p_number_unary(p):
+def p_molprop_int(p):
     """
-    number : PLUS number %prec UPLUS
-           | MINUS number %prec UMINUS
+    molprop : INDEX integer
+            | SERIAL integer
+            | RESID integer
+            | RESIDUE integer
     """
-    p[0] = ("unary", p[1], p[2])
+    p[0] = ("molprop", p[1], p[2])
 
 
-def p_expression_grouped(p):
+def p_string(p):
     """
-    expression : LPAREN expression RPAREN
+    string : ID
+           | NORMSTRING
+           | NORMSTRINGSINGLE
     """
-    p[0] = ("grouped", p[2])
+    p[0] = p[1]
 
 
 def p_number(p):
     """
-    number : INTEGER
-           | FLOAT
+    number : integer
+           | float
     """
-    p[0] = ("number", p[1])
+    p[0] = p[1]
+
+
+def p_number_unary_minus(p):
+    """
+    number : MINUS number %prec UMINUS
+    """
+    p[0] = ("uminus", p[2])
+
+
+def p_integer_unary_minus(p):
+    """
+    integer : MINUS integer %prec UMINUS
+    """
+    p[0] = ("uminus", p[2])
+
+
+def p_float_unary_minus(p):
+    """
+    float : MINUS float %prec UMINUS
+    """
+    p[0] = ("uminus", p[2])
+
+
+def p_integer(p):
+    """
+    integer : INTEGER
+    """
+    p[0] = p[1]
+
+
+def p_float(p):
+    """
+    float : FLOAT
+    """
+    p[0] = p[1]
 
 
 def p_error(p):
@@ -214,9 +446,29 @@ def p_error(p):
 parser = yacc.yacc()
 
 # Parse an expression
-ast = parser.parse("index -15")
-print(ast)
-ast = parser.parse("protein and within 8.3 of resname ACE")
-print(ast)
-ast = parser.parse("protein and (within -8.3 of resname ACE or exwithin 4 of index 2)")
-print(ast)
+selections = [
+    r"not protein",
+    r"index -15",
+    r"name 'A 1'",
+    r"chain X",
+    r"chain 'y'",
+    r"chain 0",
+    r'resname "GL"',
+    r'resname "GL\*"',
+    r"same fragment as lipid",
+    r"protein and within 8.3 of resname ACE",
+    r"protein and (within -8.3 of resname ACE or exwithin 4 of index 2)",
+    r"mass < 5",
+    r"mass = 4",
+    r"abs(-3)",
+    r"abs(charge)",
+    r"-sqr(charge)",
+    r"abs(charge) > 1",
+    r"abs(charge) <= sqr(4)",
+    r"x < 6",
+    r"x < 6 and x > 3",
+    r"sqr(x-5)+sqr(y+4)+sqr(z) > sqr(5)",
+]
+for sel in selections:
+    ast = parser.parse(sel)
+    print(f"{sel}:\n   ", ast)
