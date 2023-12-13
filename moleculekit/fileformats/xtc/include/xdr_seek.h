68b8a746ab907df02c9c0d8710239c55b8169379
Stefan Doerr <stefdoerr@gmail.com>
2023-12-13 18:14:50 +0200

    try without special case for windows

--- moleculekit/fileformats/xtc/include/xdr_seek.h
+++ moleculekit/fileformats/xtc/include/xdr_seek.h
@@ -1,13 +1,7 @@
 #ifndef _xdr_seek_h
 #define _xdr_seek_h
 
-// for int64_t on older M$ Visual Studio
-#if _MSC_VER && _MSVC_VER < 1600 && !__INTEL_COMPILER
-#include "ms_stdint.h"
-#else
 #include <stdint.h>
-#endif
-
 #include "xdrfile.h"
 
 int64_t xdr_tell(XDRFILE *xd);
