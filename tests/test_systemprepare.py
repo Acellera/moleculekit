abd4fbc7072b1b125000a013bf7b89b4d46f1b74
Stefan Doerr <stefdoerr@gmail.com>
2025-06-18 10:09:15 +0300

    pick different chains which don't cause issues on Windows/Mac

--- tests/test_systemprepare.py
+++ tests/test_systemprepare.py
@@ -192,7 +192,7 @@ def _test_nonstandard_residues(tmp_path, files):
     }
     mol = Molecule(os.path.join(test_home, inf))
     if inf == "2QRV.pdb":
-        mol.filter("chain A D K")
+        mol.filter("chain D E K M")
         mol = autoSegment2(mol, fields=("chain", "segid"))
     mol.set("chain", "W", sel="water")
 
@@ -204,7 +204,6 @@ def _test_nonstandard_residues(tmp_path, files):
     )
     pmol.fileloc.append(os.path.join(tmp_path, "prepared.pdb"))
     pmol.write(pmol.fileloc[0])
-
     _compare_results(
         os.path.join(test_home, f"{outf}.pdb"),
         os.path.join(test_home, f"{outf}.csv"),
