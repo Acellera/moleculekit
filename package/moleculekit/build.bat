3736909102a72b1fcf37587ff60be52aee5024c7
Stefan Doerr <stefdoerr@gmail.com>
2025-11-13 16:04:14 +0200

    override setuptools-scm version in conda/rattler builds

--- package/moleculekit/build.bat
+++ package/moleculekit/build.bat
@@ -1,2 +1,8 @@
+@echo on
+setlocal enabledelayedexpansion
+
+rem Inject version from rattler/conda build environment
+set SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
+
 %PYTHON% -m build -v -w --no-isolation
 for %%F in (dist\*.whl) do %PYTHON% -m pip install "%%F"
--- package/moleculekit/build.sh
+++ package/moleculekit/build.sh
@@ -1,2 +1,3 @@
+export SETUPTOOLS_SCM_PRETEND_VERSION=${PKG_VERSION}
 $PYTHON -m build -v -w --no-isolation
 $PYTHON -m pip install dist/*.whl
